f5df57da79f75000331bf3efd6e36c8c259361be
Akshay <sadalgekar.akshay95@gmail.com>
2023-10-06 16:00:07 -0500

    add indexe.js in views

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.get("/home", (req, res) => {
-    res.send("<h1>Deployment Succesfully.</h1>")
+    res.render("index.ejs")
 })
 
 app.listen(PORT, () => {
