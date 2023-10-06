b2e54555586a536836d51a7d647734b76be750f7
Akshay <sadalgekar.akshay95@gmail.com>
2023-10-06 16:24:48 -0500

    set up app.set for views

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import express from "express";
 import ejs from 'ejs';
+import path from 'path'; // Import path module to handle paths
 
+const app = express(); // Initialize Express app
 
-const app = express();
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
 const PORT = process.env.PORT || 5050;
 
 app.get("/home", (req, res) => {
     res.render("index.ejs");
-})
+});
 
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
