ac4c14e4d9f2441f70d91ade6d9e46cfff5edaa5
Akshay <sadalgekar.akshay95@gmail.com>
2023-10-06 16:35:20 -0500

    app.set add

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
-
+import ejs from 'ejs';
+import path from 'path'; // Import path module to handle paths
 
 const app = express(); // Initialize Express app
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
-
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
 
 
 const PORT = process.env.PORT || 5050;
