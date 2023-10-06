b6903a643ac41ec57078cc2e964c31a1a927e598
Akshay <sadalgekar.akshay95@gmail.com>
2023-10-06 16:52:00 -0500

    v2

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 import express from "express";
 import ejs from 'ejs';
 import path from 'path'; // Import path module to handle paths
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __dirname = dirname(fileURLToPath(
+    import.meta.url));
 
 const app = express(); // Initialize Express app
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 
-
+app.set('views', path.join(__dirname, 'views'));
 
 
 
 const PORT = process.env.PORT || 5050;
 
 app.get("/home", (req, res) => {
+    console.log(__dirname)
     res.render("index");
 });
 
